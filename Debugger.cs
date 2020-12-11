03bd41e34c94d7f9c186e0dc1d36e26852d554cb
JCxYIS <jsn811@gmail.com>
2020-12-11 16:07:10 +0800

    init

---
+++ Debugger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Debugger
+{
+    Process process = new Process();
+
+    public Debugger(string processPath, params string[] processParams)
+    { 
+        process.EnableRaisingEvents = true;
+        process.OutputDataReceived += new DataReceivedEventHandler(process_OutputDataReceived);
+        process.ErrorDataReceived += new DataReceivedEventHandler(process_ErrorDataReceived);
+        process.Exited += new EventHandler(process_Exited);
+
+        process.StartInfo.FileName = processPath;
+        process.StartInfo.Arguments = String.Join(' ', processParams);
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.RedirectStandardOutput = true;
+
+        try
+        {
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+
+            process.WaitForExit();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("UNABLE TO LAUNCH!");
+            return;
+        }
+
+    }
+
+    void process_Exited(object sender, EventArgs e)
+    {
+        Console.WriteLine( $"=== Process exited with code {process.ExitCode} ===");
+    }
+
+    void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        Console.WriteLine($"{DateTime.Now} [ERR] {e.Data}");
+    }
+
+    void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+        Console.WriteLine($"{DateTime.Now} [LOG] {e.Data}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DebugNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exe_path = @"YOUR_PROGRAM_PATH"; // e.g. E:\Downloads\a.exe
+            string exe_param = "YOUR PARAMS";       // e.g. -f -g idk
+               
+            if(args.Length > 2)
+            {
+                args[0] = exe_path;
+                args[1] = exe_param;
+            }
+
+            Debugger debugger = new Debugger(exe_path, exe_param);
+        }
+    }
+}
